01ac6c07f2f528a79af65b1f00680fcab142b29c
unknown <rachnajammu@gmail.com>
2023-10-24 16:39:28 +0530

    first commit

--- app.py
+++ app.py
@@ -1,38 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask,render_template,request
+flask1=Flask(__name__)
 
-app = Flask(__name__)
+@flask1.route("/")
 
-@app.route("/")
+#route your webpage
+
 def visitors():
 
-    # Load current count
-    counter_read_file = open("count.txt", "r")
-    visitors_count = int(counter_read_file.read())
-    counter_read_file.close()
+	# Load current count
+	counter_read_file = open("count.txt", "r")
+	visitors_count = int(counter_read_file.read())
+	counter_read_file.close()
 
-    # Increment the count
-    visitors_count = visitors_count + 1
+	# Increment the count
+	visitors_count = visitors_count + 1
 
-    # Overwrite the count
-    counter_write_file = open("count.txt", "w")
-    counter_write_file.write(str(visitors_count))
-    counter_write_file.close()
+	# Overwrite the count
+	counter_write_file = open("count.txt", "w")
+	counter_write_file.write(str(visitors_count))
+	counter_write_file.close()
 
-    # Render HTML with count variable
-    return render_template("index.html", count=visitors_count)
+	return render_template("index.html",visitors=visitors_count)
 
-@app.route('/', methods=['POST'])
+
+
+@flask1.route("/",methods=["POST"])
 def covid_stats():
-    # Load current count
-    counter_read_file = open("count.txt", "r")
-    visitors_count = int(counter_read_file.read())
-    counter_read_file.close()
+	# Load current count
+	counter_read_file = open("count.txt", "r")
+	visitors_count = int(counter_read_file.read())
+	counter_read_file.close()
+	text1=request.form["text"]
+	url="https://covidstats-sdbd.onrender.com/?country="+text1
+	return render_template("index.html",image=url,visitors=visitors_count)
 
-    text = request.form['text']
+if __name__=="__main__":
+	flask1.run()
 
-    corona_data = 'https://corona.dnsforfamily.com/graph.png?c='+text
-    print(corona_data)
-    return render_template("index.html", image=corona_data, count=visitors_count)
-
-if __name__ == "__main__":
-    app.run()
+#add code for executing flask
